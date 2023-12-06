1e200193e04a007f26ae7ddeeb50659bb6bba775
Dhritish Roy <dhritishroy@gmail.com>
2023-12-07 02:22:33 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import SignIn from './pages/SignIn'
-import SignOut from './pages/SignOut'
-import Profile from './pages/Profile'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/about' element={<About/>}/>
-    <Route path='/sign-in' element={<SignIn/>}/>
-    <Route path='/sign-out' element={<SignOut/>}/>
-    <Route path='/profile' element={<Profile/>}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- src/pages/SignOut.jsx
+++ src/pages/SignOut.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function SignOut() {
-  return (
-    <div>SignOut</div>
-  )
+  return <div>SignOut</div>;
 }
